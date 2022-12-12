364982efe576df39d6169eba622bd346d1c524f9
elishambadi <35599354+elishambadi@users.noreply.github.com>
2022-12-12 16:45:54 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# Entry point of the app
+from flask import Flask, redirect, url_for, render_template, request, flash
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/login')
+def login_page():
+    #return 'Welcome to login page';
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    #return 'Welcome to register page';
+    return render_template('register.html')
+
+@app.route('/vote')
+def vote():
+    #return 'Welcome to voting page';
+    return render_template('vote.html')
+
+@app.route('/results')
+def results():
+    #return 'Welcome to results page';
+    return render_template('results.html')
+
+#@app.route('/{username}/profile')
+#def user_profile():
+    #return 'Welcome to your profile';
+ #   return render_template('profile.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
